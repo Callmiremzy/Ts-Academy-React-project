9615068f041c1f27a711bf38b268947b75a4b10e
Akintunde-digging <isaacakintunde11@gmail.com>
2026-03-26 14:49:01 +0100

    added footer section

---
+++ planet-explorer/src/footer-folder/footer.jsx
@@ -0,0 +1,28 @@
+import "./index.css";
+
+function App() {
+  return (
+    <footer>
+      <div className="about">
+        <h3>About</h3>
+        <p>Isaac, .........</p>
+      </div>
+
+      <hr />
+
+      <div className="copyright">
+        <p>
+          ©2026 Design by Isaac A. 
+          <br className="first_br" />
+          Built by GroupName. 
+          <br className="second_br" />
+          All rights reserved
+        </p>
+
+        <p className="tsa">TSAcademy</p>
+      </div>
+    </footer>
+  );
+}
+
+export default App;
