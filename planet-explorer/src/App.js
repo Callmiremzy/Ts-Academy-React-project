11447c92d661eb89548541cc30244868cde4e4c4
Happy9ce <ogbonnayahappiness247@gmail.com>
2026-03-27 00:23:05 +0100

    Added the table section and Work in progress on video section

--- planet-explorer/src/App.js
+++ planet-explorer/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import PlanetVideo from './components/PlanetVideo/PlanetVideo';
+import PlanetTable from './components/PlanetTable/PlanetTable';
 import ContactForm from './ContactForm';  
 
 function App() {
   return (
     <div className="App">
       <PlanetVideo />
+      <PlanetTable />
       <ContactForm />
     </div>
   );
